e7fc011d8af53493f5937d6932063cf4274d1a0e
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2021-11-12 12:21:37 -0500

    go

--- Reader.cpp
+++ Reader.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include "SharedObject.h"
-
-using namespace std;
+#include "Semaphore.h"
 
 struct MyShared{
-	int sharedThreadID;
-	int sharedReportID;
-	int sharedDelay;
-	bool sharedRunning;
+	int sdelay;
+	int sthreadID;
+	int sreportID;
+	bool sRunning;
 };
+
 
 int main(void)
 {
-	cout << "I am a reader" << endl;
-	Shared<MyShared> sharedMemory("sharedMemory");
-	
-	//Reader will poll the shared memory to determine if it is in use
-	//then it will display the data. If it is not, it will break 
+	std::cout << "I am a reader" << std::endl;
+
+	Semaphore readerSem("readerSemaphore");
+	Shared<MyShared> sharedMemory("sharedMemory"); //ownership set to false when omitted
+
+	//Reader will poll the shared memory to determine if it is in use, if it is in use
+	// then it will display the data, if it is not it will break 
 	while (true){
-		if (sharedMemory->sharedRunning == true){
-			cout << "ThreadID: " << sharedMemory->sharedThreadID <<" ReportID: "<<sharedMemory->sharedReportID <<" Delay: " <<sharedMemory->sharedDelay << endl;
-			sleep(1); 
+		if (sharedMemory->sRunning == true){
+			readerSem.Wait();
+			std::cout << "ThreadID: " << sharedMemory->sthreadID <<" ReportID: "<<sharedMemory->sreportID <<" Delay: " <<sharedMemory->sdelay <<std::endl;
+		
 		}else {
 			//When a user enters n the value of Running will be set to false and the loop will break causing the reader to terminate
 			break;
